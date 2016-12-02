4a669439e0bf4f6aae616618d0a9171b4c9458b7
隈元 彰平@東京DES <kumamoto-shohei@dmm.com>
2016-12-02 19:46:52 +0900

    レスポンシブ化

--- _dev/_src/js/app.js
+++ _dev/_src/js/app.js
@@ -8,20 +8,16 @@
 			var changeScrollPos = $('.c_hdr_content').height() + $('.main_area').height();
 			var scroll = $(window).scrollTop();
             var winW = $(window).width();
-            var contentH = $('.main_content').height();
-            var asideH = $('.aside_content').height();
 
 			if(scroll > changeScrollPos){
 				$('.aside_content').css({
 					'position': 'absolute',
                     'top': scroll - changeScrollPos + 15,
-                    'bottom': 'auto',
                     'right': 0
 				});
 			}else{
 				$('.aside_content').css({
 					'position': 'relative',
-                    'bottom': 'auto',
 					'top': 'auto'
 				});
 			}
@@ -30,28 +26,16 @@
                 changeScrollPos = $('.c_hdr_content').height() + $('.main_area').height();
 				scroll = $(window).scrollTop();
 
-				if(scroll > changeScrollPos && scroll - (contentH + changeScrollPos - asideH) <= 0){
-                    // スクロール中位置固定されて追従している場合
+				if(scroll > changeScrollPos){
 					$('.aside_content').css({
 						'position': 'absolute',
                         'top': scroll - changeScrollPos + 15,
-                        'bottom': 'auto',
 						'right': 0
 					});
-				}else if(scroll > changeScrollPos && scroll - (contentH + changeScrollPos - asideH) >= 0){
-                    // 下で位置固定されている場合
-                    $('.aside_content').css({
-						'position': 'absolute',
-                        'top': 'auto',
-                        'bottom': 30,
-						'right': 0
-					});
-                }else{
-                    // 上で位置固定されていない場合
+				}else{
 					$('.aside_content').css({
 						'position': 'relative',
-						'top': 'auto',
-                        'bottom': 'auto'
+						'top': 'auto'
 					});
 				}
 			});
